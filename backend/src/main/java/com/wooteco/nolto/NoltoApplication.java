b08449568ef8ab16ba2d40547d114a8bde3692ee
pomo <bosl95@naver.com>
2021-07-07 18:08:46 +0900

    feat: backend 초기 설정

---
+++ backend/src/main/java/com/wooteco/nolto/NoltoApplication.java
@@ -0,0 +1,13 @@
+package com.wooteco.nolto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoltoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoltoApplication.class, args);
+	}
+
+}
