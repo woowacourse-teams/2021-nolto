a78e1febad2668f37199c801d334b824bcca4025
zigsong <wldms5764@gmail.com>
2021-07-08 16:33:45 +0900

    chore: styled component, storybook 세팅
    Co-authored-by: Kwon Se-jin <0307kwon@users.noreply.github.com>

---
+++ frontend/.storybook/preview.js
@@ -0,0 +1,13 @@
+import { configure } from '@storybook/react';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+configure(require.context('../src', true, /\.stories\.js?$/), module);
