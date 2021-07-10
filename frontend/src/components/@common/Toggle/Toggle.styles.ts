63556d54a4e41918ddde881c80c737e07cbf897f
zigsong <wldms5764@gmail.com>
2021-07-10 17:41:00 +0900

    feat: Toggle 컴포넌트 구현
    Co-authored-by: Kwon Se-jin <0307kwon@users.noreply.github.com>

---
+++ frontend/src/components/@common/Toggle/Toggle.styles.ts
@@ -0,0 +1,63 @@
+import styled, { css } from 'styled-components';
+
+import { PALETTE } from 'constants/palette';
+
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+const Text = styled.span`
+  font-size: 24px;
+  line-height: 24px;
+`;
+
+const Input = styled.input`
+  display: none;
+`;
+
+const switchOn = css`
+  background-color: ${PALETTE.PRIMARY_400};
+  border: none;
+
+  &::after {
+    background-color: ${PALETTE.WHITE_400};
+    transform: translateX(32px);
+  }
+`;
+
+const switchOff = css`
+  background-color: ${PALETTE.WHITE_400};
+
+  &::after {
+    background-color: ${PALETTE.PRIMARY_400};
+    transform: translateX(3px);
+  }
+`;
+
+const Toggle = styled.span<{ checked: boolean }>`
+  position: relative;
+  width: 60px;
+  height: 32px;
+  border-radius: 25px;
+  display: flex;
+  align-items: center;
+  border: 2px solid ${PALETTE.PRIMARY_400};
+
+  &::after {
+    content: '';
+    position: absolute;
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
+
+    transition: transform 0.5s ease, backgroud-color 0.5s ease;
+  }
+
+  ${({ checked }) => (checked ? switchOn : switchOff)};
+`;
+
+export default { Label, Text, Input, Toggle };
