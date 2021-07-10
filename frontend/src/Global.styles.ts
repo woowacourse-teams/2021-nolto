8802810044b2443678c12897ce6d38b6f4bc2f1a
zigsong <wldms5764@gmail.com>
2021-07-10 16:13:33 +0900

    feat: global font 설정
    Co-authored-by: Kwon Se-jin <0307kwon@users.noreply.github.com>

--- frontend/src/Global.styles.ts
+++ frontend/src/Global.styles.ts
@@ -3,11 +3,13 @@ import { createGlobalStyle } from 'styled-components';
 import { PALETTE } from './constants/palette';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap');
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: sans-serif;
+    font-family: 'Noto Sans KR', sans-serif;
     color: ${PALETTE.BLACK_400};
   }
 
