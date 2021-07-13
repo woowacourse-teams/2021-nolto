471b7e21df07f9d9d52c248ae7fed3d9ec4c7b34
권세진 <0307kwon@gmail.com>
2021-07-14 01:54:32 +0900

    refactor: Label 컴포넌트가 required를 prop으로 받도록 수정

--- frontend/src/components/@common/Label/Label.styles.ts
+++ frontend/src/components/@common/Label/Label.styles.ts
@@ -1,16 +1,20 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { PALETTE } from 'constants/palette';
 
-const Root = styled.label`
-  font-size: 24px;
-
+const asterisk = css`
   &::before {
     content: '*';
     color: ${PALETTE.RED_400};
     margin-right: 8px;
     line-height: 24px;
   }
+`;
+
+const Root = styled.label<{ required: boolean }>`
+  font-size: 24px;
+
+  ${({ required }) => required && asterisk}
 `;
 
 export default { Root };
