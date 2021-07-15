a06026428d81dca2080076f1fc85890f67ecc803
zigsong <wldms5764@gmail.com>
2021-07-15 22:20:30 +0900

    chore: http 요청을 위한 환경 세팅
    Co-authored-by: Kwon Se-jin <0307kwon@users.noreply.github.com>

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+              plugins: ['@babel/plugin-transform-runtime'],
             },
           },
         ],
