a915e3bf4423966c6cd448d71b739194b6632c96
NewWisdom <wlgp2500@gmail.com>
2021-07-16 16:20:01 +0900

    feat(): rest-docs를 위한 설정 추가

---
+++ backend/src/test/java/com/wooteco/nolto/api/ControllerTest.java
@@ -0,0 +1,62 @@
+package com.wooteco.nolto.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wooteco.nolto.auth.application.AuthService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@ActiveProfiles("test")
+@AutoConfigureRestDocs
+public class ControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private RestDocumentationContextProvider restDocumentationContextProvider;
+
+    @Autowired
+    public ObjectMapper objectMapper;
+
+    @MockBean
+    private AuthService authService;
+
+    public MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .apply(MockMvcRestDocumentation.documentationConfiguration(
+                        restDocumentationContextProvider
+                ))
+                .build();
+    }
+
+    protected static OperationRequestPreprocessor getDocumentRequest() {
+        return preprocessRequest(
+                modifyUris()
+                        .scheme("https")
+                        .host("nolto.kro.kr")
+                        .removePort(),
+                prettyPrint());
+    }
+
+    protected static OperationResponsePreprocessor getDocumentResponse() {
+        return preprocessResponse(prettyPrint());
+    }
+}
---
+++ backend/src/test/java/com/wooteco/nolto/config/RestDocsConfiguration.java
@@ -0,0 +1,17 @@
+package com.wooteco.nolto.config;
+
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@TestConfiguration
+public class RestDocsConfiguration {
+    @Bean
+    public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer() {
+        return configurer -> configurer.operationPreprocessors()
+                .withRequestDefaults(prettyPrint())
+                .withResponseDefaults(prettyPrint());
+    }
+}
