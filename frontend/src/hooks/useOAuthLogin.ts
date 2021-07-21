82e1f6df567bf555e64b8c6ec885a2eb10667680
zigsong <wldms5764@gmail.com>
2021-07-21 15:14:43 +0900

    fix: github, google OAuth 로그인 상태 저장
    Co-authored-by: Kwon Se-jin <0307kwon@users.noreply.github.com>

--- frontend/src/hooks/useOAuthLogin.ts
+++ frontend/src/hooks/useOAuthLogin.ts
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
+import useUserInfo from 'hooks/useUserInfo';
 import api from 'constants/api';
 import ROUTE from 'constants/routes';
 
 const useOAuthLogin = (type: 'google' | 'github') => {
   const history = useHistory();
   const location = useLocation();
+  const userInfo = useUserInfo();
 
   const getAccessToken = async (code: string) => {
     const { data } = await api.get<{ accessToken: string }>(
@@ -14,6 +16,9 @@ const useOAuthLogin = (type: 'google' | 'github') => {
     );
 
     localStorage.setItem('accessToken', data.accessToken);
+    userInfo.setUserInfo({
+      email: 'dummy1@email.com',
+    });
     history.push(ROUTE.HOME);
   };
 
