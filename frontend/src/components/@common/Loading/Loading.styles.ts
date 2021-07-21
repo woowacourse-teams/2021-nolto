a75ab9b733ef593d637181fd3cd85c956902c79b
zigsong <wldms5764@gmail.com>
2021-07-21 14:59:35 +0900

    feat: Loading 컴포넌트 구현
    Co-authored-by: Kwon Se-jin <0307kwon@users.noreply.github.com>

---
+++ frontend/src/components/@common/Loading/Loading.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Loading = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export default { Loading };
