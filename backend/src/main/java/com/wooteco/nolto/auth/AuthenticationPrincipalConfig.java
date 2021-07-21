4a12b06821409aefafec0665cffe058ae936826a
Gomding <backchida@gmail.com>
2021-07-22 01:25:23 +0900

    refactor: AuthenticationPrincipalConfig의 addArgumentResolvers 수정
    - 인자의 List 타입에 제네릭 타입을 지정해주지 않아 실행시 마다 경고문을 출력해서 수정
    - List -> List<HandlerMethodArgumentResolver>

--- backend/src/main/java/com/wooteco/nolto/auth/AuthenticationPrincipalConfig.java
+++ backend/src/main/java/com/wooteco/nolto/auth/AuthenticationPrincipalConfig.java
@@ -6,6 +6,7 @@ import com.wooteco.nolto.auth.ui.MemberArgumentResolver;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,7 +18,7 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     private final AuthService authService;
 
     @Override
-    public void addArgumentResolvers(List argumentResolvers) {
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(createAuthenticationPrincipalArgumentResolver());
     }
 
