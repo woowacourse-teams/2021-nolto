7debeab2e6554a852b5d1818f14e2d520d2e44fa
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-07-22 13:04:44 +0900

    feat(FeedTechService): 테크명 목록에 포함된 테크를 사용한 피드를 조회하는 기능 구현

--- backend/src/main/java/com/wooteco/nolto/feed/application/FeedTechService.java
+++ backend/src/main/java/com/wooteco/nolto/feed/application/FeedTechService.java
@@ -8,7 +8,10 @@ import com.wooteco.nolto.tech.domain.TechRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @Service
@@ -20,5 +23,23 @@ public class FeedTechService {
     public void save(Feed feed, List<Long> techIds) {
         List<Tech> techs = techRepository.findAllById(techIds);
         techs.forEach(tech -> feedTechRepository.save(new FeedTech(feed, tech)));
+    }
+
+    public Set<Feed> findFeedUsingTech(List<String> techNames) {
+        List<Tech> techs = techRepository.findAllByNameIn(techNames);
+        List<FeedTech> findFeedTech = findFeedTechByTech(techs);
+        return findFeedByFeedTech(findFeedTech);
+    }
+
+    private List<FeedTech> findFeedTechByTech(List<Tech> techs) {
+        List<FeedTech> findFeedTech = new ArrayList<>();
+        techs.forEach(tech -> findFeedTech.addAll(feedTechRepository.findByTech(tech)));
+        return findFeedTech;
+    }
+
+    private Set<Feed> findFeedByFeedTech(List<FeedTech> feedTechs) {
+        Set<Feed> findFeed = new HashSet<>();
+        feedTechs.forEach(feedTech -> findFeed.add(feedTech.getFeed()));
+        return findFeed;
     }
 }
