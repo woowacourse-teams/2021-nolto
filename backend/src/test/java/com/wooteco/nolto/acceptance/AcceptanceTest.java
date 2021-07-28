8d207ea890f0f27b1e38614fd19ebdfc46dd98c8
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-07-28 20:38:58 +0900

    test: User 인수 테스트 작성

--- backend/src/test/java/com/wooteco/nolto/acceptance/AcceptanceTest.java
+++ backend/src/test/java/com/wooteco/nolto/acceptance/AcceptanceTest.java
@@ -6,6 +6,7 @@ import com.wooteco.nolto.auth.ui.dto.TokenResponse;
 import com.wooteco.nolto.user.domain.User;
 import com.wooteco.nolto.user.domain.UserRepository;
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,17 +27,22 @@ public class AcceptanceTest {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
+    public static User 엄청난_유저 = new User(
+            "1",
+            SocialType.GITHUB,
+            "엄청난 유저",
+            "https://dksykemwl00pf.cloudfront.net/nolto-default-thumbnail.png");
+
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
     }
 
+    public RequestSpecification given() {
+        return RestAssured.given().port(port);
+    }
+
     public TokenResponse 가입된_유저의_토큰을_받는다() {
-        User 엄청난_유저 = new User(
-                "1",
-                SocialType.GITHUB,
-                "엄청난 유저",
-                "https://dksykemwl00pf.cloudfront.net/nolto-default-thumbnail.png");
         User 저장된_엄청난_유저 = userRepository.save(엄청난_유저);
 
         String token = jwtTokenProvider.createToken(String.valueOf(저장된_엄청난_유저.getId()));
