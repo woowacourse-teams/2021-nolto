73305f9d63b32dc47fabb832d5b1b9568cd8e4d3
Gomding <57378410+Gomding@users.noreply.github.com>
2021-07-29 20:12:38 +0900

    test(Tech): Tech 인수 테스트 작성 (#287)
    - auto_complete 파라미터로 기술 태그의 자동 완성 기능을 사용할 수 있다.
    - auto_complete 파라미터로 존재하지 않는 기술 태그를 조회해서 응답한다.
    - names 파라미터로 이름과 정확히 일치하는 기술 태그들을 조회해서 응답한다.
    - names 파라미터로 기술이름과 정확하게 일치하지 않으면 기술 태그를 응답하지 않는다.
    - names 파라미터로 기술이름 양 옆에 공백이 있어도 기술 이름만 일치하면 응답해준다.
    - names 파라미터에 중복된 기술이름 있으면 중복을 제외하고 반환한다.

--- backend/src/test/java/com/wooteco/nolto/acceptance/AcceptanceTest.java
+++ backend/src/test/java/com/wooteco/nolto/acceptance/AcceptanceTest.java
@@ -38,14 +38,14 @@ public class AcceptanceTest {
         RestAssured.port = port;
     }
 
-    public RequestSpecification given() {
-        return RestAssured.given().port(port);
-    }
-
     public TokenResponse 가입된_유저의_토큰을_받는다() {
         User 저장된_엄청난_유저 = userRepository.save(엄청난_유저);
 
         String token = jwtTokenProvider.createToken(String.valueOf(저장된_엄청난_유저.getId()));
         return new TokenResponse(token);
+    }
+
+    public RequestSpecification given() {
+        return RestAssured.given().port(port);
     }
 }
