2e9dcd2bd47dd3c308c46b7dbf05daa8a6506e94
zigsong <wldms5764@gmail.com>
2021-07-29 22:34:52 +0900

    chore: RTL 초기 환경설정

---
+++ frontend/src/__tests__/hooks/useHotFeeds.test.tsx
@@ -0,0 +1,19 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import { wrapper, errorHandler } from '../hookUtils';
+import useHotFeeds from 'hooks/queries/useHotFeeds';
+import { feeds } from '../fixture/Feeds';
+
+describe('인기 피드 query hook 테스트', () => {
+  it('hook 테스트', async () => {
+    const { result, waitFor } = renderHook(
+      () =>
+        useHotFeeds({
+          errorHandler,
+        }),
+      { wrapper },
+    );
+    await waitFor(() => result.current.isSuccess);
+    expect(result.current.data).toEqual(feeds);
+  });
+});
