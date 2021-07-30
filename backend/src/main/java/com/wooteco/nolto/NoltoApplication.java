4fc2fd9256af38cd1b84a9c8fab9eef8ea1bca45
NewWisdom <wlgp2500@gmail.com>
2021-07-30 21:52:26 +0900

    feat(): logging-level error에 대한 슬랙 연동 알림 재설정

--- backend/src/main/java/com/wooteco/nolto/NoltoApplication.java
+++ backend/src/main/java/com/wooteco/nolto/NoltoApplication.java
@@ -1,15 +1,18 @@
 package com.wooteco.nolto;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Slf4j
 @EnableJpaAuditing
 @SpringBootApplication
 public class NoltoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NoltoApplication.class, args);
+        log.error("==================================");
     }
 
 }
