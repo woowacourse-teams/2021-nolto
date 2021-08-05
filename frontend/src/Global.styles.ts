539d1c5c05bd52c702f3787ef698562896f4bdc8
zigsong <wldms5764@gmail.com>
2021-08-05 10:17:14 +0900

    temp for noramlize css

--- frontend/src/Global.styles.ts
+++ frontend/src/Global.styles.ts
@@ -1,8 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 import { PALETTE } from './constants/palette';
 
 const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  
   @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@1,700&display=swap');
 
