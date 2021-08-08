d900ce2c9f7a469e22384fc48fba2dfc221afc1b
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-08-08 21:15:59 +0900

    refactor: 이벤트 리스너 비동기 로직 삭제

--- backend/src/main/java/com/wooteco/nolto/notification/application/NotificationEventHandler.java
+++ backend/src/main/java/com/wooteco/nolto/notification/application/NotificationEventHandler.java
@@ -1,7 +1,6 @@
 package com.wooteco.nolto.notification.application;
 
 import lombok.AllArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -11,7 +10,6 @@ public class NotificationEventHandler {
 
     private final NotificationService notificationService;
 
-    @Async
     @TransactionalEventListener
     public void saveNotification(NotificationEvent notificationEvent) {
         notificationService.save(notificationEvent);
--- backend/src/main/java/com/wooteco/nolto/notification/application/NotificationService.java
+++ backend/src/main/java/com/wooteco/nolto/notification/application/NotificationService.java
@@ -8,6 +8,7 @@ import com.wooteco.nolto.notification.domain.NotificationRepository;
 import com.wooteco.nolto.user.domain.User;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class NotificationService {
 
     private final NotificationRepository notificationRepository;
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void save(NotificationEvent notificationEvent) {
         Notification notification = notificationEvent.toEntity();
         notificationRepository.save(notification);
