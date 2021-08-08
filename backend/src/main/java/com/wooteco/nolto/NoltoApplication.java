4310c56c34bd3c75460e525e6fa16dbaead90a92
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-08-08 21:17:44 +0900

    refactor: 이벤트 리스너 비동기 로직 삭제 (#400)
    * test: 히스토리 인수 테스트
    
    - AcceptanceTest의 userRepository public으로 접근 제한자 변경
    - userRepository를 활용하여 필요한 댓글/피드 정보 저장하여 테스트
    
    * refactor(UserAcceptanceTest): assertThat().extracting()을 활용한 필드 추출로 변경
    
    * fix: 빠뜨린 테스트 코드 수정
    
    * refactor: 이벤트 리스너 비동기 로직 삭제

--- backend/src/main/java/com/wooteco/nolto/NoltoApplication.java
+++ backend/src/main/java/com/wooteco/nolto/NoltoApplication.java
@@ -3,9 +3,7 @@ package com.wooteco.nolto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication
 public class NoltoApplication {
