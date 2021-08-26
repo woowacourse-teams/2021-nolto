93ef90ed57ce1c951baec323511a00d69e55258b
zigsong <wldms5764@gmail.com>
2021-08-26 13:34:57 +0900

    refactor: ErrorFallback 디자인 변경

--- frontend/src/components/ErrorFallback/ErrorFallback.styles.ts
+++ frontend/src/components/ErrorFallback/ErrorFallback.styles.ts
@@ -1,45 +1,67 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
+import HorseIcon from 'assets/horse.svg';
 import { PALETTE } from 'constants/palette';
 
 const Root = styled.div`
+  width: fit-content;
   display: flex;
   flex-direction: column;
   align-items: center;
   position: relative;
   font-family: 'Work Sans', sans-serif;
-  color: ${PALETTE.RED_500};
-`;
-
-const Image = styled.img`
-  border-radius: 50%;
-  filter: drop-shadow(0 0 1rem rgba(0, 0, 0, 0.25));
-`;
-
-const Blur = styled.div`
-  filter: blur(4px);
-  position: absolute;
-  width: 100%;
+  color: ${PALETTE.PRIMARY_400};
 `;
 
 const Message = styled.div`
+  display: flex;
+  padding: 0 0.75rem;
+  gap: 2rem;
   color: inherit;
-  position: absolute;
-  text-align: center;
-  top: 40%;
+  transform: translateY(15%);
 `;
 
-const ErrorText = styled.span`
+const ErrorTitle = styled.div`
   color: inherit;
   font-weight: 900;
   font-style: italic;
-  font-size: 4rem;
+  font-size: 4.5rem;
+  transform: translateY(10%);
 `;
 
-const ErrorDetail = styled.pre`
+const falldown = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+
+  50% {
+    transform: rotate(240deg);
+  }
+
+  100% {
+    transform: rotate(180deg);
+  }
+`;
+
+const Horse = styled(HorseIcon)`
+  width: 72px;
+  height: auto;
+
+  animation: ${falldown} 1s ease forwards;
+`;
+
+const Divider = styled.hr`
+  width: 100%;
+  background-color: ${PALETTE.PRIMARY_400};
+  height: 4px;
+  border: none;
+`;
+
+const ErrorText = styled.div`
   color: inherit;
-  font-size: 1.5rem;
-  margin-top: 1rem;
+  font-size: 1.25rem;
+  margin-top: 0.5rem;
+  padding: 0 0.75rem;
 `;
 
-export default { Root, Image, Blur, Message, ErrorText, ErrorDetail };
+export default { Root, Message, ErrorTitle, Divider, ErrorText, Horse };
