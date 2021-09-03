de67769e2f4900b0735358bb1f91c78f3ae0e207
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-09-03 09:13:10 +0900

    refactor: 페치 조인 도입하여 N+1 문제 해결

--- backend/src/main/java/com/wooteco/nolto/feed/domain/repository/FeedRepository.java
+++ backend/src/main/java/com/wooteco/nolto/feed/domain/repository/FeedRepository.java
@@ -19,12 +19,14 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
 
     @Query(value = "select feed " +
             "from Feed as feed " +
+            "join fetch feed.author u " +
             "where feed.id <= :feedId and feed.step in :steps " +
             "order by feed.createdDate desc, feed.id desc")
     List<Feed> findWithoutHelp(@Param("steps") EnumSet<Step> steps, @Param("feedId") Long feedId, Pageable pageable);
 
     @Query(value = "select feed " +
             "from Feed as feed " +
+            "join fetch feed.author u " +
             "where feed.id <= :feedId and feed.step in :steps and feed.isSos =:help " +
             "order by feed.createdDate desc, feed.id desc")
     List<Feed> findWithHelp(@Param("steps") EnumSet<Step> steps, @Param("help") Boolean help, @Param("feedId") Long feedId, Pageable pageable);
