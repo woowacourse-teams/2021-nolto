1e12615c4eefa70bd002fbd4783fad47ddbd5b27
권세진 <0307kwon@gmail.com>
2021-09-09 18:02:55 +0900

    chore: api endpoint가 환경변수에 따라 변경 가능

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -6,7 +6,7 @@ export const BASE_URL: { [key: string]: string } = {
 };
 
 const api = axios.create({
-  baseURL: BASE_URL[process.env.NODE_ENV] || BASE_URL.development,
+  baseURL: BASE_URL[process.env.BASE_URL] || BASE_URL.development,
 });
 
 api.interceptors.request.use(
