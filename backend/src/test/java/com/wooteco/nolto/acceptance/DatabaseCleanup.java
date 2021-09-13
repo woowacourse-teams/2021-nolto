c74f59951022382ea52165a1ccb9dda2a883cea2
NewWisdom <wlgp2500@gmail.com>
2021-09-13 23:38:27 +0900

    chore(): 포맷팅 적용

--- backend/src/test/java/com/wooteco/nolto/acceptance/DatabaseCleanup.java
+++ backend/src/test/java/com/wooteco/nolto/acceptance/DatabaseCleanup.java
@@ -36,16 +36,16 @@ class DatabaseCleanup implements InitializingBean {
         tableNames.addAll(tableNamesWithAnnotation);
     }
 
-@Transactional
-public void execute() {
-    entityManager.flush();
-    entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+    @Transactional
+    public void execute() {
+        entityManager.flush();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
 
-    for (String tableName : tableNames) {
-        entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
-        entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+        for (String tableName : tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+        }
+
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
     }
-
-    entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
-}
 }
