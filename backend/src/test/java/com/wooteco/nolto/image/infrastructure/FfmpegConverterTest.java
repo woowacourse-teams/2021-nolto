2334eef576a42612abf766f34f4bbf822fa2eb2f
gomding <backchida@gmail.com>
2021-09-14 06:07:47 +0900

    test(FfmpegConverterTest): gif -> mp4 변환 테스트 작성

---
+++ backend/src/test/java/com/wooteco/nolto/image/infrastructure/FfmpegConverterTest.java
@@ -0,0 +1,34 @@
+package com.wooteco.nolto.image.infrastructure;
+
+import com.wooteco.nolto.image.config.FfmpegConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {FfmpegConverter.class, FfmpegConfig.class})
+class FfmpegConverterTest {
+
+    private final String gifFileName = "jjv1FK.gif";
+
+    @Autowired
+    private FfmpegConverter ffmpegConverter;
+
+    @Test
+    void convertGifToMp4() {
+        // given
+        URL resource = getClass().getClassLoader().getResource(gifFileName);
+        String gifFilePath = resource.getPath();
+        String expectedPath = "/Users/parksmac/IdeaProjects/2021-nolto/backend/build/resources/test/static/" + gifFileName;
+        assertThat(gifFilePath).isEqualTo(expectedPath);
+        int indexOfExtensionDot = gifFilePath.lastIndexOf(".");
+        String filePathWithoutExtension = gifFilePath.substring(0, indexOfExtensionDot);
+        String mp4FilePath = filePathWithoutExtension + ".mp4";
+
+        // when
+        ffmpegConverter.convertGifToMp4(gifFilePath, mp4FilePath);
+    }
+}
