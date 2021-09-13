c6a889d51d28a526f966812a152ae9f06704db5e
pomo <bosl95@naver.com>
2021-09-13 17:00:58 +0900

    refactor: 하드 코딩 데이터 상수화

--- backend/src/main/java/com/wooteco/nolto/TomcatWebCustomConfig.java
+++ backend/src/main/java/com/wooteco/nolto/TomcatWebCustomConfig.java
@@ -7,8 +7,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TomcatWebCustomConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 
+    public static final String SPECIAL_CHARACTER = "<>[\\]^`{|}";
+    public static final String QUERY_PARAM_PROPERTY = "relaxedQueryChars";
+
     @Override
     public void customize(TomcatServletWebServerFactory factory) {
-        factory.addConnectorCustomizers(connector -> connector.setProperty("relaxedQueryChars", "<>[\\]^`{|}"));
+        factory.addConnectorCustomizers(connector -> connector.setProperty(QUERY_PARAM_PROPERTY, SPECIAL_CHARACTER));
     }
 }
