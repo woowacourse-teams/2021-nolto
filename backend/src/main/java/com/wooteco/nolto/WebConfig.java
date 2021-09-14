f632ee3676185b3a827fbeca4010e6940d86c18c
NewWisdom <wlgp2500@gmail.com>
2021-09-14 17:08:14 +0900

    fix(): api 문서 resource 경로 허용

--- backend/src/main/java/com/wooteco/nolto/WebConfig.java
+++ backend/src/main/java/com/wooteco/nolto/WebConfig.java
@@ -3,6 +3,7 @@ package com.wooteco.nolto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods("*")
                 .allowedOriginPatterns(allowOrigin);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**").addResourceLocations("classpath:/static/docs/");
     }
 }
