d479e62052778f520b37b7027e08dd45a3618e68
zigsong <wldms5764@gmail.com>
2021-09-29 16:27:13 +0900

    feat: ssr 템플릿에 styleTag 주입
    Co-authored-by: Kwon Se-jin <0307kwon@users.noreply.github.com>

--- frontend/server/index.tsx
+++ frontend/server/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import express from 'express';
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 import path from 'path';
 import fs from 'fs';
@@ -10,15 +11,20 @@ import App from '../src/App';
 
 const PORT = process.env.PORT || 9000;
 const app = express();
+const sheet = new ServerStyleSheet();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   const reactApp = ReactDOMServer.renderToString(
-    <StaticRouter location={req.url}>
-      <App />
-    </StaticRouter>,
+    <StyleSheetManager sheet={sheet.instance}>
+      <StaticRouter location={req.url}>
+        <App />
+      </StaticRouter>
+    </StyleSheetManager>,
   );
+
+  const styleTags = sheet.getStyleTags();
 
   const indexFile = path.resolve(__dirname, '../index.html');
 
@@ -28,7 +34,11 @@ app.get('/', (req, res) => {
       return res.status(500).send('서버에서 에러가 발생했습니다. 🔫');
     }
 
-    return res.send(data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`));
+    const result = data
+      .replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+      .replace(/<head>(.+)<\/head>/s, `<head>$1 ${styleTags}</head>`);
+
+    return res.send(result);
   });
 });
 
