fd541f76e562b597d83b7bfaef614556e78c91a1
pomo <bosl95@naver.com>
2021-10-04 15:37:18 +0900

    feat: refresh token 재발급 기능 추가 - redis 적용 - errorType 추가   - INVALID_CLIENT : 권한 없는 클라이언트의 요청인 경우

--- backend/src/test/java/com/wooteco/nolto/auth/application/AuthServiceTest.java
+++ backend/src/test/java/com/wooteco/nolto/auth/application/AuthServiceTest.java
@@ -2,10 +2,12 @@ package com.wooteco.nolto.auth.application;
 
 import com.wooteco.nolto.auth.domain.OAuthClientProvider;
 import com.wooteco.nolto.auth.domain.SocialType;
+import com.wooteco.nolto.auth.infrastructure.RedisUtil;
 import com.wooteco.nolto.auth.infrastructure.oauth.GithubClient;
 import com.wooteco.nolto.auth.infrastructure.oauth.GoogleClient;
 import com.wooteco.nolto.auth.ui.dto.OAuthRedirectResponse;
 import com.wooteco.nolto.auth.ui.dto.OAuthTokenResponse;
+import com.wooteco.nolto.auth.ui.dto.RefreshTokenRequest;
 import com.wooteco.nolto.auth.ui.dto.TokenResponse;
 import com.wooteco.nolto.exception.BadRequestException;
 import com.wooteco.nolto.exception.ErrorType;
@@ -33,6 +35,7 @@ class AuthServiceTest {
     public static final String USER_NICKNAME = "user";
     private static final User USER1 = new User("socialId1", SocialType.GITHUB, USER_NICKNAME, "imageUrl");
     private static final User USER2 = new User("socialId2", SocialType.GITHUB, USER_NICKNAME, "imageUrl");
+    private static final String CLIENT_IP_V6 = "0:0:0:0:0:0:0:1";
 
     @Autowired
     private AuthService authService;
@@ -48,6 +51,9 @@ class AuthServiceTest {
 
     @MockBean
     private GoogleClient googleClient;
+
+    @MockBean
+    private RedisUtil redisUtil;
 
     @DisplayName("깃허브 로그인의 code를 얻기위한 파라미터들을 요청한다.")
     @Test
@@ -85,8 +91,7 @@ class AuthServiceTest {
         given(githubClient.generateAccessToken("code")).willReturn(OAUTH_TOKEN_RESPONSE1);
         given(githubClient.generateUserInfo(OAUTH_TOKEN_RESPONSE1)).willReturn(USER1);
 
-        TokenResponse tokenResponse = authService.oAuthSignIn("github", "code");
-
+        TokenResponse tokenResponse = authService.oAuthSignIn("github", "code", CLIENT_IP_V6);
         assertThat(tokenResponse).isNotNull();
     }
 
@@ -97,8 +102,7 @@ class AuthServiceTest {
         given(githubClient.generateAccessToken("code")).willReturn(OAUTH_TOKEN_RESPONSE1);
         given(githubClient.generateUserInfo(OAUTH_TOKEN_RESPONSE1)).willReturn(USER1);
 
-        TokenResponse tokenResponse = authService.oAuthSignIn("google", "code");
-
+        TokenResponse tokenResponse = authService.oAuthSignIn("google", "code", CLIENT_IP_V6);
         assertThat(tokenResponse).isNotNull();
     }
 
@@ -109,11 +113,11 @@ class AuthServiceTest {
         given(githubClient.generateAccessToken("code")).willReturn(OAUTH_TOKEN_RESPONSE1);
         given(githubClient.generateUserInfo(OAUTH_TOKEN_RESPONSE1)).willReturn(USER1);
 
-        assertThatThrownBy(() -> authService.oAuthSignIn("google", null))
+        assertThatThrownBy(() -> authService.oAuthSignIn("google", null, CLIENT_IP_V6))
                 .isInstanceOf(BadRequestException.class)
                 .hasMessage(ErrorType.INVALID_OAUTH_CODE.getMessage());
 
-        assertThatThrownBy(() -> authService.oAuthSignIn("google", ""))
+        assertThatThrownBy(() -> authService.oAuthSignIn("google", "", CLIENT_IP_V6))
                 .isInstanceOf(BadRequestException.class)
                 .hasMessage(ErrorType.INVALID_OAUTH_CODE.getMessage());
     }
@@ -125,7 +129,7 @@ class AuthServiceTest {
         given(githubClient.generateAccessToken("code")).willReturn(OAUTH_TOKEN_RESPONSE1);
         given(githubClient.generateUserInfo(OAUTH_TOKEN_RESPONSE1)).willReturn(USER1);
 
-        assertThatThrownBy(() -> authService.oAuthSignIn("naver", "code"))
+        assertThatThrownBy(() -> authService.oAuthSignIn("naver", "code", CLIENT_IP_V6))
                 .isInstanceOf(BadRequestException.class)
                 .hasMessage(ErrorType.NOT_SUPPORTED_SOCIAL_LOGIN.getMessage());
     }
@@ -142,8 +146,8 @@ class AuthServiceTest {
         User existNickNameUser = new User("socialId3", SocialType.GITHUB, USER_NICKNAME, "imageUrl");
         userRepository.save(existNickNameUser);
 
-        authService.oAuthSignIn("google", "code1");
-        authService.oAuthSignIn("google", "code2");
+        authService.oAuthSignIn("google", "code1", CLIENT_IP_V6);
+        authService.oAuthSignIn("google", "code2", CLIENT_IP_V6);
 
         boolean 맨처음_저장된_중복_닉네임_존재여부 = userRepository.existsByNickName(USER_NICKNAME);
         boolean 두번째_저장된_중복_닉네임_존재여부 = userRepository.existsByNickName(USER_NICKNAME + "(1)");
@@ -152,5 +156,20 @@ class AuthServiceTest {
         assertThat(맨처음_저장된_중복_닉네임_존재여부).isTrue();
         assertThat(두번째_저장된_중복_닉네임_존재여부).isTrue();
         assertThat(세번째_저장된_중복_닉네임_존재여부).isTrue();
+    }
+
+    @DisplayName("리프레시 토큰을 이용해 액세스/리프레시 토큰을 재발급 한다.")
+    @Test
+    void refreshToken() {
+        // given
+        given(redisUtil.get("refresh token")).willReturn("client IP");
+
+        // when
+        TokenResponse tokenResponse = authService.refreshToken(new RefreshTokenRequest(1L, "refresh token", "client IP"));
+
+        // then
+        assertThat(tokenResponse.getRefreshToken()).isNotNull();
+        assertThat(tokenResponse.getAccessToken()).isNotNull();
+        assertThat(tokenResponse.getExpiredIn()).isNotNull();
     }
 }
