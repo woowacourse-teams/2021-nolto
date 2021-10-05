b95d28e44c037689520fd9ea9de17dd9fcd3a360
Gomding <57378410+Gomding@users.noreply.github.com>
2021-10-05 14:56:19 +0900

    [BE] refactor: FFmpeg Path 설정 애플리케이션이 관리하도록 수정 (#590)
    * feat: ffmpeg를 애플리케이션에 넣기위한 의존성 추가
    
    implementation 'org.bytedeco:ffmpeg:4.4-1.5.6'
    implementation 'org.bytedeco:javacpp:1.5.6'
    implementation 'org.bytedeco:javacv-platform:1.5.6'
    
    * refactor: 애플리케이션이 관리하는 ffmpeg를 사용하도록 수정
    
    * refactor(ImageService): method 분리
    
    * refactor(ImageResizeService): ImageSize 도메인을 사용하도록 수정
    
    * refactor(ImageResizeService): 변수명 변경 및 메서드 분리
    
    * refactor: 각 test 메서드에 DisplayName 추가
    
    * refactor(ErrorType): 에러메세지 오타 추가
    
    * refactor(ImageSize): 예외 발생시 log 찍히도록 추가
    
    * refactor(FFmpegConverterTest): 예외상황 테스트 추가

--- backend/src/main/java/com/wooteco/nolto/image/infrastructure/FFmpegConverter.java
+++ backend/src/main/java/com/wooteco/nolto/image/infrastructure/FFmpegConverter.java
@@ -6,10 +6,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
-import net.bramp.ffmpeg.FFprobe;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
 import net.bramp.ffmpeg.job.FFmpegJob;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,12 +23,12 @@ public class FFmpegConverter {
     private static final String DELETE_SOUND_OPTION = "-an";
 
     private final FFmpeg ffmpeg;
-    private final FFprobe ffprobe;
 
     public void convertGifToMp4(String gifFilePath, String mp4FilePath) {
         log.info("convert gif to mp4 {} -> {}", gifFilePath, mp4FilePath);
         try {
-            ImageSize gifImageSizeWithResize = ImageSize.resizeOf(gifFilePath);
+            checkExistsGifFile(gifFilePath);
+            ImageSize resizedGifImage = ImageSize.of(gifFilePath).resize();
 
             FFmpegBuilder builder = new FFmpegBuilder()
                     .setInput(gifFilePath)
@@ -36,14 +39,31 @@ public class FFmpegConverter {
                     .setVideoMovFlags("faststart")
                     .setVideoPixelFormat("yuv420p")
                     .setVideoFilter("scale=trunc(iw/2)*2:trunc(ih/2)*2")
-                    .setVideoWidth(gifImageSizeWithResize.getWidthOnesRounded())
-                    .setVideoHeight(gifImageSizeWithResize.getHeightOnesRounded())
+                    .setVideoWidth(resizedGifImage.getWidthOnesRounded())
+                    .setVideoHeight(resizedGifImage.getHeightOnesRounded())
                     .done();
 
-            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg);
             FFmpegJob job = executor.createJob(builder);
             job.run();
-        } catch (RuntimeException e) {
+            checkExistsMp4File(mp4FilePath);
+
+        } catch (IOException e) {
+            log.error("gif파일을 mp4로 변환하는데 실패했습니다.", e);
+            throw new InternalServerErrorException(ErrorType.GIF_MP4_CONVERT_FAIL);
+        }
+    }
+
+    private void checkExistsGifFile(String gifFilePath) {
+        if (Files.notExists(Paths.get(gifFilePath))) {
+            log.error("gif파일이 경로에 존재하지 않습니다.");
+            throw new InternalServerErrorException(ErrorType.GIF_MP4_CONVERT_FAIL);
+        }
+    }
+
+    private void checkExistsMp4File(String mp4FilePath) {
+        if (Files.notExists(Paths.get(mp4FilePath))) {
+            log.error("gif파일 변환 후 mp4파일이 존재하지 않습니다.");
             throw new InternalServerErrorException(ErrorType.GIF_MP4_CONVERT_FAIL);
         }
     }
--- backend/src/test/java/com/wooteco/nolto/image/infrastructure/FFmpegConverterTest.java
+++ backend/src/test/java/com/wooteco/nolto/image/infrastructure/FFmpegConverterTest.java
@@ -1,24 +1,44 @@
 package com.wooteco.nolto.image.infrastructure;
 
+import com.wooteco.nolto.exception.InternalServerErrorException;
 import com.wooteco.nolto.image.config.FFmpegConfig;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@Disabled
 @SpringBootTest(classes = {FFmpegConverter.class, FFmpegConfig.class})
 class FFmpegConverterTest {
 
     private final String gifFileName = "jjv1FK.gif";
+    private final String mp4FileName = "jjv1FK.mp4";
 
     @Autowired
     private FFmpegConverter ffmpegConverter;
 
+    @AfterEach
+    void tearDown() throws IOException {
+        변환_후_생성된_파일삭제();
+    }
+
+    private void 변환_후_생성된_파일삭제() throws IOException {
+        URL resource = getClass().getClassLoader().getResource("static/" + mp4FileName);
+        if (Objects.nonNull(resource)) {
+            Files.delete(Paths.get(resource.getPath()));
+        }
+    }
+
+    @DisplayName("ffmpeg 명령어를 사용해서 gif파일을 mp4 파일로 변환한다.")
     @Test
     void convertGifToMp4() {
         // given
@@ -29,6 +49,38 @@ class FFmpegConverterTest {
         String mp4FilePath = filePathWithoutExtension + ".mp4";
 
         // when
-        assertDoesNotThrow(() -> ffmpegConverter.convertGifToMp4(gifFilePath, mp4FilePath));
+        ffmpegConverter.convertGifToMp4(gifFilePath, mp4FilePath);
+
+        // then
+        URL mp4URL = getClass().getClassLoader().getResource("static/" + mp4FileName);
+        assertThat(mp4URL.getPath()).isEqualTo(mp4FilePath);
+    }
+
+    @DisplayName("gif파일이 파일 경로에 존재하지 않으면 예외가 발생한다. ")
+    @Test
+    void convertGifToMp4WithInvalidGifFilePath() {
+        // given
+        String gifFilePath = "static/매우잘못된경로에있는파일.gif";
+        int indexOfExtensionDot = gifFilePath.lastIndexOf(".");
+        String filePathWithoutExtension = gifFilePath.substring(0, indexOfExtensionDot);
+        String mp4FilePath = filePathWithoutExtension + ".mp4";
+
+        // when then
+        assertThatThrownBy(() -> ffmpegConverter.convertGifToMp4(gifFilePath, mp4FilePath))
+                .isInstanceOf(InternalServerErrorException.class)
+                .hasMessage("gif파일을 mp4파일로 변환에 실패하였습니다.");
+    }
+
+    @DisplayName("gif파일이 파일 경로에 존재하지 않으면 예외가 발생한다. ")
+    @Test
+    void convertGifToMp4WithInvalidMp4FilePath() {
+        // given
+        URL resource = getClass().getClassLoader().getResource("static/" + gifFileName);
+        String gifFilePath = resource.getPath();
+        String mp4FilePath = "static/이런경로가없는데/매우잘못된경로에있는파일.mp4";
+
+        // when then
+        assertThatThrownBy(() -> ffmpegConverter.convertGifToMp4(gifFilePath, mp4FilePath))
+                .isInstanceOf(RuntimeException.class);
     }
 }
