ef9f144ebae1597ec570c8b747f46dbc0a5e7fcf
권세진 <0307kwon@gmail.com>
2021-10-07 14:14:58 +0900

    chore: SSR에 필요한 패키지 설치

--- frontend/global.d.ts
+++ frontend/global.d.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export {};
 
 declare global {
   interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     Kakao: any;
+    __REACT_QUERY_STATE__: any;
   }
 }
