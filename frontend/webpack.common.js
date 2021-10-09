414811c0ec4e4e0e4847088ab8175a866b6c85b0
권세진 <0307kwon@gmail.com>
2021-10-09 12:58:52 +0900

    fix: img의 onError 이벤트가 서버에서 동작하지 않는 문제 수정

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -24,7 +24,7 @@ module.exports = {
               presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
               plugins: [
                 '@babel/plugin-transform-runtime',
-                ['babel-plugin-styled-components', { namespace: 'nolto' }],
+                'babel-plugin-styled-components',
                 '@loadable/babel-plugin',
               ],
             },
