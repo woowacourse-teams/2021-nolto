337c36c010509c03de2c5468dbb644f2723f6949
pomo <34594339+bosl95@users.noreply.github.com>
2021-10-15 10:45:20 +0900

    refactor: userid를 redis에서 저장하도록 수정 (#627)
    * refactor: userid를 redis에서 저장하도록 수정
    
    * refactor: redisutil -> RedisRepository 수정

---
+++ backend/src/main/java/com/wooteco/nolto/auth/infrastructure/RedisRepository.java
@@ -0,0 +1,39 @@
+package com.wooteco.nolto.auth.infrastructure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class RedisRepository {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public RedisRepository(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public String leftPop(String key) {
+        return redisTemplate.opsForList().leftPop(key);
+    }
+
+    public boolean exist(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+    public void set(String key, String value, String value2, long expiredIn) {
+        log.info("[ value1 : " + value + "] Save Redis Value.");
+        redisTemplate.opsForList().leftPush(key, value);
+        log.info("[ value2 : " + value2 + "] Save Redis Value.");
+        redisTemplate.opsForList().leftPush(key, value2);
+        redisTemplate.expire(key, expiredIn, TimeUnit.SECONDS);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+}
+
