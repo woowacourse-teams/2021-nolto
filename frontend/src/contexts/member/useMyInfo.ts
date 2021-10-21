3fa7bff036d5ca48f4e6e85a790758827635fa34
권세진 <0307kwon@gmail.com>
2021-10-22 00:48:07 +0900

    fix: refresh token이 일찍 만료되는 현상 수정

--- frontend/src/contexts/member/useMyInfo.ts
+++ frontend/src/contexts/member/useMyInfo.ts
@@ -9,16 +9,16 @@ import HttpError from 'utils/HttpError';
 import { ErrorHandler, UserInfo } from 'types';
 
 interface CustomQueryOption extends UseQueryOptions<UserInfo, HttpError> {
-  accessToken: string;
+  accessTokenValue: string;
   errorHandler?: ErrorHandler;
 }
 
-export const getMember = async ({ accessToken, errorHandler }: CustomQueryOption) => {
-  if (!accessToken) {
+export const getMember = async ({ accessTokenValue, errorHandler }: CustomQueryOption) => {
+  if (!accessTokenValue) {
     throw new CustomError('로그아웃 상태입니다.');
   }
 
-  backendApi.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+  backendApi.defaults.headers.common['Authorization'] = `Bearer ${accessTokenValue}`;
 
   try {
     const { data } = await backendApi.get('/members/me');
@@ -33,21 +33,21 @@ export const getMember = async ({ accessToken, errorHandler }: CustomQueryOption
   }
 };
 
-const useMyInfo = ({ accessToken, errorHandler, ...option }: CustomQueryOption) => {
+const useMyInfo = ({ accessTokenValue, errorHandler, ...option }: CustomQueryOption) => {
   const queryClient = useQueryClient();
 
   const { data, refetch } = useQuery<UserInfo>(
     QUERY_KEYS.MEMBER,
-    () => getMember({ accessToken, errorHandler }),
+    () => getMember({ accessTokenValue, errorHandler }),
     option,
   );
 
   useEffect(() => {
-    if (!accessToken) return;
+    if (!accessTokenValue) return;
 
     queryClient.cancelQueries(QUERY_KEYS.MEMBER);
     refetch();
-  }, [accessToken]);
+  }, [accessTokenValue]);
 
   return { data, refetch };
 };
