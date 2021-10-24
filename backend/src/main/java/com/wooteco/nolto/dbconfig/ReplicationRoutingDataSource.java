236abb932e3a41a134df76c54311f66970384bf1
gomding <backchida@gmail.com>
2021-10-24 19:23:07 +0900

    refactor(ReplicationRoutingDataSource.class): N개의 Replica 서버 설정값을 Routing 할 수 있도록 수정
    조회 요청마다 0~N 개를 순차적으로 사용
    ex. 3개의 slave DB 서버를 운용 시
    1번째 요청 -> slave1 DB 서버
    2번째 요청 -> slave2 DB 서버
    3번째 요청 -> slave3 DB 서버
    4번째 요청 -> slave1 DB 서버

--- backend/src/main/java/com/wooteco/nolto/dbconfig/ReplicationRoutingDataSource.java
+++ backend/src/main/java/com/wooteco/nolto/dbconfig/ReplicationRoutingDataSource.java
@@ -4,20 +4,45 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.util.List;
+
 @Slf4j
 public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
 
-    public static final String DATASOURCE_MASTER_KEY = "master";
-    public static final String DATASOURCE_SLAVE_KEY = "slave";
+    public static final String DATASOURCE_SOURCE_KEY = "source";
+
+    private ReplicaDataSourceNames replicaDataSourceNames;
+
+    public void setReplicaDataSourceNames(List<String> names) {
+        this.replicaDataSourceNames = new ReplicaDataSourceNames(names);
+    }
 
     @Override
     protected Object determineCurrentLookupKey() {
         final boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
         if (isReadOnly) {
-            log.info("use slave datasource");
-            return DATASOURCE_SLAVE_KEY;
+            String nextReplicaDataSourceName = replicaDataSourceNames.getNextName();
+            log.info("Using Replica DB Name : {}", nextReplicaDataSourceName);
+            return nextReplicaDataSourceName;
         }
-        log.info("use master datasource");
-        return DATASOURCE_MASTER_KEY;
+        log.info("Using Source DB");
+        return DATASOURCE_SOURCE_KEY;
+    }
+
+    public static class ReplicaDataSourceNames {
+
+        private List<String> values;
+        private int counter = 0;
+
+        public ReplicaDataSourceNames(List<String> values) {
+            this.values = values;
+        }
+
+        public String getNextName() {
+            if (counter == values.size()) {
+                counter = 0;
+            }
+            return values.get(counter++);
+        }
     }
 }
