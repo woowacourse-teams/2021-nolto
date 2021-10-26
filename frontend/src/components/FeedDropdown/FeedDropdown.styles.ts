3ff45ccf7033fdfa53e60288a619b84356e131f5
Kwon Se-jin <48755175+0307kwon@users.noreply.github.com>
2021-10-26 12:08:01 +0900

    [FE] refactor: 반응형 일부 수정 (#664)
    * fix: 모바일, 태블릿 환경에서는 글 수정 버튼 삭제
    
    * refactor: 피드 상세 페이지에서의 step chip 그림자, hover 제거

--- frontend/src/components/FeedDropdown/FeedDropdown.styles.ts
+++ frontend/src/components/FeedDropdown/FeedDropdown.styles.ts
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
 
 import { PALETTE } from 'constants/palette';
+import { MEDIA_QUERY } from 'constants/mediaQuery';
+import { Z_INDEX } from 'constants/styles';
 
 const Root = styled.div`
-  & > .delete-button {
+  z-index: ${Z_INDEX.DROPDOWN};
+
+  @media ${MEDIA_QUERY.TABLET} {
+    & .modify-button {
+      display: none;
+    }
+  }
+
+  & .delete-button {
     color: ${PALETTE.RED_400};
   }
 `;
